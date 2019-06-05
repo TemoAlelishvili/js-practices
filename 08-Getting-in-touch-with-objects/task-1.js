5a1f2d7d8f2e26c10b1fe6b3d8cf364ba090ef6a
temoalelishvili <temoaleilishvili@yahoo.com>
2019-06-05 21:21:50 +0400

    fix(08-Getting-in-touch-with-objects): Fix task 1

--- 08-Getting-in-touch-with-objects/task-1.js
+++ 08-Getting-in-touch-with-objects/task-1.js
@@ -2,10 +2,7 @@ const person = {
     get salary() {
         const today = new Date();
         const amountofday = new Date(today.getFullYear(), today.getMonth()+1, 0).getDate();
-        console.log(amountofday);
         return amountofday - today.getDate() > 20 ? 'good salary' : 'bad salary';
     }
 }
 console.log(person.salary);
-
-
