ebd06ffda738cccc1d5bfb1cca140c7f1a0b0639
temoalelishvili <temoaleilishvili@yahoo.com>
2019-06-06 11:38:30 +0400

    feat(08-Getting-in-touch-with-objects): Finish task-2

---
+++ 08-Getting-in-touch-with-objects/task-2.js
@@ -0,0 +1,28 @@
+const person = {};
+Object.defineProperties(person, {
+    'rate' : {
+        value: 0,
+        writable: true,
+        configurable: false,
+        enumerable:false,
+
+    },
+    'salary' :{
+        get () {
+            let day = new Date().getDate();    
+            let sum = day * person.rate;
+    
+            return sum
+        },
+
+        set (value) {
+            throw new Error('You can\'t change salary');
+        },
+
+        
+    }
+});
+person.rate = 30;
+// person.salary = 6;
+
+console.log(person.salary);
