e886106df7104219ea37cbb073095e9c37844767
temoalelishvili <temoaleilishvili@yahoo.com>
2019-06-07 21:05:49 +0400

    feat(09-Expanded-Object-Functionality): Finish tasks
    n

---
+++ 09-Expanded-Object-Functionality/task-1.js
@@ -0,0 +1,22 @@
+Object.prototype.extend = function(o) 
+{
+    for (let prop of Object.keys(o))
+    {
+        if (typeof this[prop] === 'undefined')
+        {
+            this[prop] = o[prop];
+        }
+        for (let prop in Object.keys(o)) {
+            if (Object.keys(o).hasOwnProperty(prop)) {
+                Object[prop] = Object.keys(o)[prop];
+            }
+        }
+    }
+}
+const data = { a: 'a' };
+const source = { a: 'A', b: 'b' };
+Object.defineProperty(source, 'b', { writable: false });
+data.extend(source);
+console.log(data); // { a: 'a', b: 'b' }
+console.log(Object.getOwnPropertyDescriptor(data, 'b').writable); // false
+
